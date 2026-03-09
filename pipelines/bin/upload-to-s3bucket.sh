a7be385666abce56f826d0e1b3f4e3e3da67c9c1
Bob Coret <bob.coret+github@gmail.com>
2026-03-09 19:47:53 +0100

    Update upload script to use DATASET variable, update dataset description registration

--- pipelines/bin/upload-to-s3bucket.sh
+++ pipelines/bin/upload-to-s3bucket.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+# to prevent docker creating 'root' owned files
 source setuser.sh
 
-# pass the dataset name as the first argument
-export DATASETNAME="${1}"
-export DATASET="./${DATASETNAME}"
+# read the configuration variables for this dataset
+source environment
+
+if [ -z $DATASET ]; then
+  echo "Please swith to a dataset directory" 
+fi
+
+# TODO move to environment
 export S3_BUCKET="nde-europeana"
 
 if [ ! -d .s3 ] || [ ! -f .s3/.s3cfg ]; then
@@ -34,12 +40,17 @@ fi
 
 echo "Uploading ${DATASET} files to S3 bucket ..."
 
-echo "- ${DATASETNAME}.zip"
-docker compose run --rm s3cmd --config=/root/.s3cfg -f --cf-invalidate --no-preserve --no-mime-magic --mime-type=application/zip --acl-public put /s3/${DATASETNAME}.zip s3://${S3_BUCKET}/${1}.edmxml.zip
-echo "- ${DATASETNAME}.nt.gz"
-docker compose run --rm s3cmd -f --cf-invalidate --no-preserve --no-mime-magic --mime-type=application/n-triples+gzip --acl-public put ${DATASETNAME}.nt.gz s3://${S3_BUCKET}/${DATASETNAME}.nt.gz
-echo "- ${DATASETNAME}.datasetdescription.ttl"
-docker compose run --rm s3cmd -f --cf-invalidate --no-preserve --no-mime-magic --mime-type=text/turtle --acl-public put datasetdescription.ttl s3://${S3_BUCKET}/${DATASETNAME}.datasetdescription.ttl
+echo "- ${DATASET}.zip"
+docker compose run --rm s3cmd --config=/root/.s3cfg -f --cf-invalidate --no-preserve --no-mime-magic --mime-type=application/zip --acl-public put /s3/${DATASET}.zip s3://${S3_BUCKET}/${DATASET}.edmxml.zip
+echo "- ${DATASET}.nt.gz"
+docker compose run --rm s3cmd -f --cf-invalidate --no-preserve --no-mime-magic --mime-type=application/n-triples+gzip --acl-public put ${DATASET}.nt.gz s3://${S3_BUCKET}/${DATASET}.nt.gz
+echo "- ${DATASET}.datasetdescription.ttl"
+docker compose run --rm s3cmd -f --cf-invalidate --no-preserve --no-mime-magic --mime-type=text/turtle --acl-public put datasetdescription.ttl s3://${S3_BUCKET}/${DATASET}.datasetdescription.ttl
 
-echo "Registering dataset description https://${S3_BUCKET}.ams3.digitaloceanspaces.com/${DATASETNAME}.datasetdescription.ttl"
-curl 'https://datasetregister.netwerkdigitaalerfgoed.nl/api/datasets' -H 'link: <http://www.w3.org/ns/ldp#RDFSource>; rel="type",<http://www.w3.org/ns/ldp#Resource>; rel="type"' -H 'content-type: application/ld+json' --data-binary '{"@id":"https://${S3_BUCKET}.ams3.digitaloceanspaces.com/${DATASETNAME}.datasetdescription.ttl"}'
+echo "Registering dataset description https://${S3_BUCKET}.ams3.digitaloceanspaces.com/${DATASET}.datasetdescription.ttl"
+curl -v -X 'POST' \
+  'https://datasetregister.netwerkdigitaalerfgoed.nl/api/datasets' \
+  -H 'accept: application/ld+json' \
+  -H 'Content-Type: application/ld+json' \
+  -d '{ "@id": "https://'$S3_BUCKET'.ams3.digitaloceanspaces.com/'$DATASET'.datasetdescription.ttl" }'
+#curl 'https://datasetregister.netwerkdigitaalerfgoed.nl/api/datasets' -H 'link: <http://www.w3.org/ns/ldp#RDFSource>; rel="type",<http://www.w3.org/ns/ldp#Resource>; rel="type"' -H 'content-type: application/ld+json' --data-binary '{"@id":""}'
